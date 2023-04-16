0a4b37b87c7135190dc670e06d8a3cd368c30ed8
anant0059 <2019uce0059@gmail.com>
2023-04-16 21:57:02 +0530

    Redirecting Request

--- Section 3 - Understanding the Basics/Node - Server/app.js
+++ Section 3 - Understanding the Basics/Node - Server/app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+
 
 // function rqListener(req, res) {
 
@@ -20,8 +22,32 @@ const http = require('http');
 // });
 
 /////////////// routing response
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     if ( url === '/' ) {
+//         res.write('<html>');
+//         res.write('<head><title>Enter Message</title></head>');
+//         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+//         res.write('</html>');
+//         return res.end();
+//     }
+//     console.log(req.url, req.method, req.headers);
+//     // process.exit();
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page.</title></head>');
+//     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//     res.write('</html>');
+//     res.end();
+// });
+
+
+
+//////////////////// redirecting requests
+
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if ( url === '/' ) {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
@@ -29,6 +55,13 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     console.log(req.url, req.method, req.headers);
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
