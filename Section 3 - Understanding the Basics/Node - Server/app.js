1a58610920dc0bdfa69a341af3c69cc2ccc24590
anant0059 <2019uce0059@gmail.com>
2023-04-16 21:45:28 +0530

    routing response

--- Section 3 - Understanding the Basics/Node - Server/app.js
+++ Section 3 - Understanding the Basics/Node - Server/app.js
@@ -6,7 +6,29 @@ const http = require('http');
 
 // http.createServer(rqListener);
 
+
+/////////// simple request and response
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers);
+//     // process.exit();
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page.</title></head>');
+//     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//     res.write('</html>');
+//     res.end();
+// });
+
+/////////////// routing response
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if ( url === '/' ) {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     console.log(req.url, req.method, req.headers);
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
@@ -16,5 +38,6 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     res.end();
 });
+
 
 server.listen(3000);
