42a8c7eb822c598b9766b2ecb825dcc97a7a73cb
anant0059 <2019uce0059@gmail.com>
2023-06-10 00:26:01 +0530

    Parsing request bodies

--- Section 3 - Understanding the Basics/Node - Server/app.js
+++ Section 3 - Understanding the Basics/Node - Server/app.js
@@ -45,6 +45,37 @@ const fs = require('fs');
 
 //////////////////// redirecting requests
 
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     if ( url === '/' ) {
+//         res.write('<html>');
+//         res.write('<head><title>Enter Message</title></head>');
+//         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+//         res.write('</html>');
+//         return res.end();
+//     }
+//     if (url === '/message' && method === 'POST') {
+//         fs.writeFileSync('message.txt', 'DUMMY');
+//         res.statusCode = 302;
+//         res.setHeader('Location', '/');
+//         return res.end();
+//     }
+
+//     console.log(req.url, req.method, req.headers);
+//     // process.exit();
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page.</title></head>');
+//     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//     res.write('</html>');
+//     res.end();
+// });
+
+
+
+///////////////////// Parsing request bodies
+
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
@@ -56,13 +87,26 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        }); //listen to ceratain events i.e. data here
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
     }
 
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -71,6 +115,4 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     res.end();
 });
-
-
 server.listen(3000);
