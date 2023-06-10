5b2f1dcb9da32c01dffb8f9f69d997e1d70d6352
anant0059 <2019uce0059@gmail.com>
2023-06-10 23:34:14 +0530

    Handeling different routes

--- Section 5 - Working with  Express.js/app.js
+++ Section 5 - Working with  Express.js/app.js
@@ -1,18 +1,38 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next(); //Allows the request to continue to the next middleware in line
+
+////////////// Middleware
+
+// app.use((req, res, next) => {
+//     console.log("In the middleware");
+//     next(); //Allows the request to continue to the next middleware in line
+// });
+
+// app.use((req, res, next) => {
+//     console.log("In another middleware");
+//     res.send('<h1>Hello Form Express</h1>');
+// });
+
+
+/////////////// Handeling different routes
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("In another middleware");
+    res.send('<h1>Add product page</h1>');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log("In another middleware");
+    res.send('<h1>Hello Form Express</h1>');
+});
 
-server.listen(3001);
+// const server = http.createServer(app);
+
+app.listen(3001);  // this will aoutomatically return http.createServer(app);
