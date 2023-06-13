469ca02af8adc5181317982e72189ebb70e0175a
anant0059 <2019uce0059@gmail.com>
2023-06-13 21:33:44 +0530

    pug added dynamically;

--- Secton 6 - Working with Dynamic Content & Adding Templating Engines/routes/admin.js
+++ Secton 6 - Working with Dynamic Content & Adding Templating Engines/routes/admin.js
@@ -9,9 +9,9 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
     // console.log("In another middleware");
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 
-    res.render('add-product', {pageTitle:'Add Product'});
+    res.render('add-product', {pageTitle:'Add Product', path: '/admin/add-product'});
 });
 
 router.post('/add-product', (req, res, next) => {
