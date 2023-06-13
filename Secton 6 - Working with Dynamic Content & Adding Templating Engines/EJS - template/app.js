516b3907f078cf2713a9f2c8b25e9f137be82fa8
anant0059 <2019uce0059@gmail.com>
2023-06-14 00:10:19 +0530

    EJS added

---
+++ Secton 6 - Working with Dynamic Content & Adding Templating Engines/EJS - template/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+// const rootDir = require('./util/path');
+
+const app = express();
+
+
+app.set('view engine', 'ejs'); //set ejs as default templating engine
+app.set('views', 'views'); //set views as default path for the ejs files
+
+const adminData = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+
+    res.status(404).render('404', {pageTitle: "Page Not Found"});
+})
+
+// const server = http.createServer(app);
+
+app.listen(3000);  // this will aoutomatically return http.createServer(app);
---
+++ Secton 6 - Working with Dynamic Content & Adding Templating Engines/EJS - template/routes/admin.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+    // console.log("In another middleware");
+    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+
+    res.render('add-product', {
+        pageTitle:'Add Product', 
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formCSS: true
+    });
+});
+
+router.post('/add-product', (req, res, next) => {
+    // console.log(req.body);
+    products.push({title: req.body.title});
+    res.redirect('/');
+});
+
+// module.exports = router;
+
+exports.routes = router;
+exports.products = products;
