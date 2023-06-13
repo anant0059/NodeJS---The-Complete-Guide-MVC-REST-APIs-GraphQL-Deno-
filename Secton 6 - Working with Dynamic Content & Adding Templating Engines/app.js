e32340e29b993fa4a47fb99b0b90e2a8def133f5
anant0059 <2019uce0059@gmail.com>
2023-06-13 20:57:04 +0530

    pub-template added by removing HTML files

--- Secton 6 - Working with Dynamic Content & Adding Templating Engines/app.js
+++ Secton 6 - Working with Dynamic Content & Adding Templating Engines/app.js
@@ -6,84 +6,23 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'pug'); //set pug as default templating engine
+app.set('views', 'views'); //set views as default path for the pub files
 
-////////////// Middleware
-
-// app.use((req, res, next) => {
-//     console.log("In the middleware");
-//     next(); //Allows the request to continue to the next middleware in line
-// });
-
-// app.use((req, res, next) => {
-//     console.log("In another middleware");
-//     res.send('<h1>Hello Form Express</h1>');
-// });
-
-
-/////////////// Handeling different routes
-
-// app.use('/', (req, res, next) => {
-//     console.log('This always runs!');
-//     next();
-// });
-
-// app.use('/add-product', (req, res, next) => {
-//     console.log("In another middleware");
-//     res.send('<h1>Add product page</h1>');
-// });
-
-// app.use('/', (req, res, next) => {
-//     console.log("In another middleware");
-//     res.send('<h1>Hello Form Express</h1>');
-// });
-
-// // const server = http.createServer(app);
-
-// app.listen(3001);  // this will aoutomatically return http.createServer(app);
-
-
-
-
-// /////////////// Parsing incoming request
-
-// app.use(bodyParser.urlencoded({extended: false}));
-
-// app.use('/', (req, res, next) => {
-//     // console.log('This always runs!');
-//     next();
-// });
-
-// app.use('/add-product', (req, res, next) => {
-//     // console.log("In another middleware");
-//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>');
-// });
-
-// app.post('/product', (req, res, next) => {
-//     console.log(req.body);
-//     res.redirect('/');
-// });
-
-// app.use('/', (req, res, next) => {
-//     // console.log("In another middleware");
-//     res.send('<h1>Hello Form Express</h1>');
-// });
-
-
-
-///////////////// Using express router
-
-const adminRoutes = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+
+    res.status(404).render('404');
 })
 
 // const server = http.createServer(app);
