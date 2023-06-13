9aa846d95dc9dcbb26affd1abb9aa6027ed696c3
anant0059 <2019uce0059@gmail.com>
2023-06-13 23:41:18 +0530

    handlebars added, work dynamically

--- Secton 6 - Working with Dynamic Content & Adding Templating Engines/Handlebars-template/routes/admin.js
+++ Secton 6 - Working with Dynamic Content & Adding Templating Engines/Handlebars-template/routes/admin.js
@@ -11,7 +11,13 @@ router.get('/add-product', (req, res, next) => {
     // console.log("In another middleware");
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 
-    res.render('add-product', {pageTitle:'Add Product', path: '/admin/add-product'});
+    res.render('add-product', {
+        pageTitle:'Add Product', 
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formCSS: true
+    });
 });
 
 router.post('/add-product', (req, res, next) => {
