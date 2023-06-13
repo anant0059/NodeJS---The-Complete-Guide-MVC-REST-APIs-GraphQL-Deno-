55b6965cc651ecc4ad9f4be022520a88396ba090
anant0059 <2019uce0059@gmail.com>
2023-06-14 03:02:21 +0530

    product model, fetching data in and from files

--- Section 7 - The Model View Controller (MVC)/controllers/product.js
+++ Section 7 - The Model View Controller (MVC)/controllers/product.js
@@ -1,4 +1,6 @@
-const products = [];
+// const products = [];
+
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     // console.log("In another middleware");
@@ -15,7 +17,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     // console.log(req.body);
-    products.push({title: req.body.title});
+    // products.push({title: req.body.title});
+
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
@@ -24,12 +29,26 @@ exports.getProducts = (req, res, next) => {
     // console.log('shop.js', adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 
-    res.render('shop', {
-        prods: products, 
-        pageTitle: "Shop", 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    // const products = Product.fetchAll();
+    
+    // res.render('shop', {
+    //     prods: products, 
+    //     pageTitle: "Shop", 
+    //     path: '/', 
+    //     hasProducts: products.length > 0,
+    //     activeShop: true,
+    //     productCSS: true
+    // });
+
+
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: "Shop", 
+            path: '/', 
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
-}
+};
