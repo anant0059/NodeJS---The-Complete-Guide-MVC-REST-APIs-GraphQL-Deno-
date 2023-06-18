2cc9bea489e155eb97c0f8e2a2abda200bf2555b
anant0059 <2019uce0059@gmail.com>
2023-06-18 22:06:40 +0530

    section - 9 init

---
+++ Section 9 - Dynamic Routes & Advanced Models/models/product.js
@@ -0,0 +1,74 @@
+// const products = [];
+
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path'); 
+const p = path.join(rootDir, 
+    'data', 
+    'products.json'
+);
+
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            cb([])
+        }
+        else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
+module.exports = class product {
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        // products.push(this);
+
+        // const p = path.join(rootDir, 
+        //     'data', 
+        //     'products.json'
+        // );
+
+        // fs.readFile(p, (err, fileContent) => {
+        //     let products = [];
+        //     if(!err) {
+        //         products = JSON.parse(fileContent)
+        //     }
+        //     products.push(this);
+        //     fs.writeFile(p, JSON.stringify(products), (err) =>{
+        //         console.log(err);
+        //     });
+        // });
+
+        getProductsFromFile( products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(cb) {
+        // const p = path.join(rootDir, 
+        //     'data', 
+        //     'products.json'
+        // );
+    
+        // fs.readFile(p, (err, fileContent) => {
+        //     if(err) {
+        //         cb([])
+        //     }
+        //     cb(JSON.parse(fileContent));
+        // });
+
+        getProductsFromFile(cb);
+    }
+}
