e98ff1e4412dfcad31d3bc44a7d02919ff939c40
anant0059 <2019uce0059@gmail.com>
2023-07-03 23:24:54 +0530

    databased server added and connected

--- Section 10 - SQL Introduction/app.js
+++ Section 10 - SQL Introduction/app.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database');
+
 const app = express();
 
 
@@ -14,6 +16,13 @@ app.set('views', 'views'); //set views as default path for the ejs files
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
+db.execute('SELECT * FROM products')
+    .then(result => {
+        console.log(result[0], result[1]);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
